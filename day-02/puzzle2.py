b85087e29b9c4ff8f4c0db1592d54ecafe94d324
Matthew <aquidmatthewla@gmail.com>
2024-12-02 22:39:58 +0000

    day-02

---
+++ day-02/puzzle2.py
@@ -0,0 +1,39 @@
+import os
+
+script_dir = os.path.dirname(__file__)
+test_file_path = os.path.join(script_dir, "test.txt")
+input_file_path = os.path.join(script_dir, "input.txt")
+
+
+def solve(filename):
+    with open(filename, 'r') as f:
+        tests = f.readlines()
+    tests = [l.strip().split() for l in tests]
+    c = 0
+    for rep in tests:
+        rep = [int(l) for l in rep]
+        if check_increase(rep) or check_decrease(rep):
+            c += 1
+        else:
+            for i in range(len(rep)):
+                t = rep[:i] + rep[i+1:]
+                if check_increase(t) or check_decrease(t):
+                    c+=1
+                    break   
+    return c
+    
+    
+def check_increase(report):
+    for i in range(len(report)-1):
+        if not (report[i] < report[i+1] and 1 <= report[i+1]-report[i] <= 3):
+            return False
+    return True
+
+def check_decrease(report):
+    for i in range(len(report)-1):
+        if not (report[i] > report[i+1] and 1 <= report[i]-report[i+1] <= 3):
+                return False
+    return True
+
+print(f"Test Solution: {solve(test_file_path)}")
+print(f"Question Solution: {solve(input_file_path)}")
